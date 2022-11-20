56e0be6036c8fbf06e8aec3a4e1fd2fb4c514521
vhdive-br <henrique.murat9@gmail.com>
2022-11-20 05:11:40 -0300

    Primeiras alterações

--- calculadora.py
+++ calculadora.py
@@ -6,19 +6,20 @@ escolha = input('''
 [ 1 ] Adição(Addition)
 [ 2 ] Subtração(Subtraction)
 [ 3 ] Divisão(Division)
-[ 4 ] Multiplicação(Product)''')
-soma = 0
+[ 4 ] Multiplicação(Product)
+Pressione/Press "Enter" ''')
 while True:
+    soma = 0
     menu = int(input('O que deseja? (Choose your option): '))
     sleep(1)
     print('Ok! Digite 0 para parar a calculadora (Enter 0 to stop the calculator).')
     while menu == 1:
         valor = int(input('Digite o valor (enter the value): '))
-        if valor == 0:
-            print(escolha)
         soma = soma + valor
         print(soma)
+        if valor == 0:
+            break
     
-    if menu == 0:
+    if menu == 0:1
         break
-print('fim')
+print('Fim! Obrigado volte sempre/Thank you and welcome back!')
