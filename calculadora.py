e3f6d8cf4d250d05fc67e41d92121e48e29cdd76
vhdive-br <henrique.murat9@gmail.com>
2022-11-20 05:00:12 -0300

    Primeiras linhas

--- calculadora.py
+++ calculadora.py
@@ -1,1 +1,24 @@
-print('Teste')
+from time import sleep
+print('=' * 30,'Olá, bem vindo! Escolha sua opção desejada abaixo.','=' * 30)
+print('=' * 31, 'Hello welcome! Choose your desired option below.','=' * 31)
+sleep(1)
+escolha = input('''
+[ 1 ] Adição(Addition)
+[ 2 ] Subtração(Subtraction)
+[ 3 ] Divisão(Division)
+[ 4 ] Multiplicação(Product)''')
+soma = 0
+while True:
+    menu = int(input('O que deseja? (Choose your option): '))
+    sleep(1)
+    print('Ok! Digite 0 para parar a calculadora (Enter 0 to stop the calculator).')
+    while menu == 1:
+        valor = int(input('Digite o valor (enter the value): '))
+        if valor == 0:
+            print(escolha)
+        soma = soma + valor
+        print(soma)
+    
+    if menu == 0:
+        break
+print('fim')
